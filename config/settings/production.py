96b53e724ff4cdbd5854647073c965afcdd45788
OmarEhab177 <omarehap17@gmail.com>
2021-07-24 16:17:14 +0200

    remove secret key from production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,8 +4,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY",
-    default="YzIcCzyuqEOZDUQ59ghHk0fy0V9SnjyeJK2b4np85YfEPlIo1JAUQ5hRAaEMGCXS",)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com", "143.244.179.162", 'localhost'])
 
