a1decb7f54c0264a75451d9c4407fb3ed084a880
OmarEhab177 <omarehap17@gmail.com>
2021-07-25 14:02:10 +0200

    make CSRF_COOKIE_SECURE and SESSION_COOKIE_SECURE equal False

--- config/settings/production.py
+++ config/settings/production.py
@@ -36,9 +36,9 @@ CACHES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 # SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)   # commented this 
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
