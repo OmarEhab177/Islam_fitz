d89fd00e00e617b3304e5b591e015b08b6c4a7a5
OmarEhab177 <omarehap17@gmail.com>
2021-08-22 00:52:20 +0200

    some changes

--- islam_fitz/core/models.py
+++ islam_fitz/core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class BeforeAndAfter(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=255, default=1)
+    client_name = models.CharField(max_length=255, default=1)
     image = models.ImageField(upload_to = 'photos/before_and_after/%y/%m/%d')
     feature = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
@@ -15,7 +15,7 @@ class BeforeAndAfter(models.Model):
         verbose_name_plural = 'BeforeAndAfter'
 
     def __str__(self):
-        return self.description
+        return self.client_name
 
 
 class AboutUs(models.Model):
